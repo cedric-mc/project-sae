74f8bef55efd8ed933162ed5d90652336e6a7485
MARIYA CONSTANTINE CEDRIC <mariyaconsta02@heckel02.u-pem.fr>
2023-09-22 11:00:43 +0200

    double formulaire

--- index.php
+++ index.php
@@ -1,11 +1,36 @@
 <html>
+    <head>
+        <meta charset="utf-8">
+        <title>Création d'un compte pour le joueur ou connexion</title>
+    </head>
+
     <body>
         <header>
-            <h1>Création d'un compte pour le joueur</h1>
+            <h1>Création d'un compte pour le joueur ou connexion</h1>
         </header>
         <main>
-            <form action="" method="POST">
+            <h2>Création d'un compte</h2>
+            <form action="validation_mail.php" method="POST">
+                <label for="pseudo">Pseudo</label>
+                <input type="text" name="pseudo" id="pseudo" required><br>
+                <label for="year">Année de naissance</label>
+                <input type="year" name="year" id="year" required><br>
+                <label for="email">Email</label>
+                <input type="email" name="email" id="email" required><br>
+                <label for="password">Mot de passe</label>
+                <input type="password" name="password" id="password" required>
+                <br><br>
+                <input type="submit" value="Envoyer">
+            </form>
 
+            <h2>Connexion</h2>
+            <form action="auth.php" method="POST">
+                <label for="pseudo">Pseudo</label>
+                <input type="text" name="pseudo" id="pseudo" required><br>
+                <label for="password">Mot de passe</label>
+                <input type="password" name="password" id="password" required>
+                <br><br>
+                <input type="submit" value="Envoyer">
             </form>
         </main>
     </body>
