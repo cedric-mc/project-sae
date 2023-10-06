5646560efdb3b81e34462ff9d0b5f892b1af228e
MARIYA CONSTANTINE CEDRIC <mariyaconsta02@heckel02.u-pem.fr>
2023-10-06 10:01:04 +0200

    10

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php
+session_start();
+?>
 <html>
     <head>
         <meta charset="utf-8">
@@ -11,49 +14,46 @@
         <main>
             <h2>Création d'un compte</h2>
             <?php
-            $yearError = "";
-            $emailError = "";
-            $passwordError = "";
-            if ($_SERVER["REQUEST_METHOD"] == "POST") {
-                $year = $_POST["year"];
-                $email = $_POST["email"];
-                $password = $_POST["password"];
-
-                $currentYear = date("Y");
-                if ($year < 1900 || $year > $currentYear && strlen($year) != 4) {
-                    $yearError = "Année de naissance invalide.";
-                }
-                if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-                    $emailError = "L'adresse e-mail n'est pas au format valide.";
-                }
-                if (strlen($password) < 12) {
-                    $passwordError = "Le mot de passe doit contenir au moins 12 caractères.";
-                }
-                if (!preg_match('/[0-9]/', $password)) {
-                    $passwordError = "Le mot de passe doit contenir au moins un chiffre.";
-                }
-                if (!preg_match('/[A-Z]/', $password)) {
-                    $passwordError = "Le mot de passe doit contenir au moins une majuscule.";
-                }
-                if (!preg_match('/[a-z]/', $password)) {
-                    $passwordError = "Le mot de passe doit contenir au moins une minuscule.";
-                }
-                if (!preg_match('/[^a-zA-Z0-9]/', $password)) {
-                    $passwordError = "Le mot de passe doit contenir au moins un caractère spécial.";
-                }
+            if(isset($_SESSION['pseudo'])){
+                $pseudoError = $_SESSION['pseudoError'];
+            } else{
+                $pseudoError = "";
+            }
+            if(isset($_SESSION['year'])){
+                $yearError = $_SESSION['yearError'];
+            } else{
+                $yearError = "";
+            }
+            if(isset($_SESSION['email'])){
+                $emailError = $_SESSION['emailError'];
+            } else{
+                $emailError = "";
+            }
+            if(isset($_SESSION['password'])){
+                $passwordError = $_SESSION['passwordError'];
+            } else{
+                $passwordError = "";
             }
             ?>
             <form action="validation_mail.php" method="POST">
                 <label for="pseudo">Pseudo</label>
-                <input type="text" name="pseudo" id="pseudo" required><br>
+                <input type="text" name="pseudo" id="pseudo" required>
+                <br>
                 <label for="year">Année de naissance</label>
-                <input type="number" name="year" id="year" required><br>
+                <input type="number" name="year" id="year" required>
+                <br>
                 <label for="email">Email</label>
-                <input type="email" name="email" id="email" required><br>
+                <input type="email" name="email" id="email" required>
+                <br>
                 <label for="password">Mot de passe</label>
                 <input type="password" name="password" id="password" required>
+                <br>
                 <br><br>
-                <input type="submit" value="Envoyer">
+                <input type="submit" value="Envoyer"><br>
+                <span style="color: red;"><?php echo $pseudoError; ?></span>
+                <span style="color: red;"><?php echo $yearError; ?></span>
+                <span style="color: red;"><?php echo $emailError; ?></span>
+                <span style="color: red;"><?php echo $passwordError; ?></span>
             </form>
 
             <h2>Connexion</h2>
